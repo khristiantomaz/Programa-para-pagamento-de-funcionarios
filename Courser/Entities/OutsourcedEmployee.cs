e566bc7c26c0ea0f935a26f437017e6839c88a48
khristian tomaz <khristiantomaz@icloud.com>
2023-08-28 18:17:59 -0300

    Adicionar arquivos de projeto.

---
+++ Courser/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities
+{
+    internal class OutsourcedEmployee : Employee
+    {
+        
+        public double Additionacharge { get; set; }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHours, double additionacharge)
+            : base(name, hours, valuePerHours)
+        {
+            Additionacharge = additionacharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * Additionacharge;
+
+        }
+
+    }
+}
---
+++ Courser/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Course.Entities;
+using System.Globalization;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+
+namespace Course
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee> ();
+            Console.Write("Enter the number of employees");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsourced (Y/N)?");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                String name = Console.ReadLine();
+                int hours = int.Parse(Console.ReadLine());
+                Console.WriteLine("Value per hour: ");
+                double valuerPerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (ch == 'y')
+                {
+                    Console.Write("Additional charge:");
+                    double additonalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuerPerHour, additonalCharge));
+                }
+
+                else
+                {
+                    list.Add(new Employee(name, hours, valuerPerHour));
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("PAYMENTS:");
+                foreach(Employee emp in list) 
+                {
+                    Console.WriteLine(emp.Name + "- $" + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+                }
+
+            }
+        }
+        
+    }
+}
